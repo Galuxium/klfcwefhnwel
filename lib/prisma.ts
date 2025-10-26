26cdae7d6996ef6eb762198081773abbbd94c7d6
Galuxium <cofounder.galuxium@gmail.com>
2025-10-26 23:40:41 +0530

    Add lib/prisma.ts

---
+++ lib/prisma.ts
@@ -0,0 +1,13 @@
+// prisma.ts
+
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  var prisma: null | PrismaClient;
+}
+
+const client = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
+
+export default client;
